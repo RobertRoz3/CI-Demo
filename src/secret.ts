67b2cba93041fa1520ff723c00347d063ece0fa4
Robert <32210201+RobertRoz3@users.noreply.github.com>
2023-09-12 14:22:40 -0400

    Update secret.ts

--- src/secret.ts
+++ src/secret.ts
@@ -13,7 +13,7 @@
 // 7, 4 -> 3
 // All inputs must be >= 1
 export function secret_1(_num1: number, _num2: number): number {
-    return 0;
+    return _num1 % _num2;
 }
 
 // Implements the following input to output mapping:
@@ -23,7 +23,7 @@ export function secret_1(_num1: number, _num2: number): number {
 // 7, 4 -> 2401
 // All inputs must be >= 1
 export function secret_2(_num1: number, _num2: number): number {
-    return 0;
+    return _num1 ** _num2;
 }
 
 // Implements the following input to output mapping:
@@ -33,7 +33,7 @@ export function secret_2(_num1: number, _num2: number): number {
 // 256 -> 16
 // All inputs must be >= 0
 export function secret_3(_num1: number): number {
-    return 0;
+    return sqrt(_num1);
 }
 
 // Implements the following input to output mapping:
@@ -43,7 +43,11 @@ export function secret_3(_num1: number): number {
 // 7 -> 5040
 // All inputs must be >= 0
 export function secret_4(_num1: number): number {
-    return 0;
+    let val = 1;
+    for (i = 2; i <= _num1; i++){
+        val *= i;
+    }
+    return val;
 }
 
 // Implements the following input to output mapping:
@@ -53,5 +57,11 @@ export function secret_4(_num1: number): number {
 // 7 -> 13
 // All inputs must be >= 1
 export function secret_5(_num1: number): number {
-    return 0;
+    if (_num1 <= 1) return _num1;
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= _num1; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
 }
